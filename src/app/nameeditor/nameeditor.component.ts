7a163f545d6767f083f9e7587205f9902d845d18
ghanlohar <gslohar@gmail.com>
2019-07-27 23:35:52 +0530

    profile editor modifications

--- src/app/nameeditor/nameeditor.component.ts
+++ src/app/nameeditor/nameeditor.component.ts
@@ -4,18 +4,18 @@ import { FormControl } from '@angular/forms';
 @Component({
   selector: 'app-nameeditor',
   templateUrl: './nameeditor.component.html',
-  styleUrls: ['./nameeditor.component.css']
+
 })
 export class NameeditorComponent implements OnInit {
-  
+  name = new FormControl('');
+  currentVal = '';
   constructor() { }
 
   ngOnInit() {
+    this.name.valueChanges.subscribe((value) => {console.log(value); this.currentVal = value; } );
   }
-  name = new FormControl('test');
+
   updateName() {
     this.name.setValue('Nancy');
   }
-  getValue() {
-    this.name.valueChanges.subscribe((value) => {console.log(value);return value;})}
 }
